66247a65e5bf202fdde836882be74fa8979e7273
Aomx <julian.cordes@gmail.com>
2015-07-16 17:15:27 +0200

    added perl script for simple executing of the complete test suite or selected subfolders.

---
+++ testRunner.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+use File::Find;
+
+sub println {
+	my ($string) = @_;
+	print "$string\n";
+}
+
+sub isTestscript {
+	my ($fileName) = @_;
+	return $fileName =~  m|(.*)\.pkt|;
+}
+
+sub getTestFilesFromDir {
+	my ($dirWithTests) = @_;
+	my @testFiles = ();
+
+	my $findCallback = sub {
+		my $fileName = $_;
+		if (isTestscript($fileName)) {
+			my %testScript = ();
+			$testScript{'fileName'} = $fileName;
+			$testScript{'absolutePath'} = File::Spec->rel2abs($fileName);
+			push(@testFiles, \%testScript);
+		}
+	};
+
+	find($findCallback, $dirWithTests);
+	@testFilesSorted = sort {return ${$a}{'fileName'} cmp ${$b}{'fileName'};} (@testFiles);
+	return @testFilesSorted;
+}
+
+sub executeTestScript {
+	my ($scriptPath) = @_;
+	my $command = "packetdrill " . $scriptPath;
+	if (system($command) == 0) {
+		println "OK";
+	}
+}
+
+my $dirWithTests= ".";
+
+if (defined $ARGV[0]) {
+	$dirWithTests = $ARGV[0];
+}
+
+my @testFiles = getTestFilesFromDir($dirWithTests);
+
+foreach $testScriptRef (@testFiles) {
+	my %testScript = %{$testScriptRef};
+	println " --- " . $testScript{'fileName'} . " ---";
+	executeTestScript($testScript{'absolutePath'});
+}
